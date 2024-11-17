3a15249dbc48849aaf3acdc82fc90ea68abb8855
serenachiu <serena.chiu.bear@gmail.com>
2024-11-17 11:26:43 +0800

    Add files via upload
    add timestamp seconds

--- xmas-card-2024/xmas-card.js
+++ xmas-card-2024/xmas-card.js
@@ -691,8 +691,10 @@ function createDownloadHTML() {
     
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        
     
-        return `${day}/${month}/${year} ${hours}:${minutes}`;
+        return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
     }    
 
     const downloadHTML =
